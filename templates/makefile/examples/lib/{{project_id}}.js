2605e5e856fb150696e9e636f80937eb75c8f2ff
Cliffano Subagio <cliffano@gmail.com>
2026-09-12 10:18:54 +1000

    Include examples lib/. Was accidentally ignored.

---
+++ templates/makefile/examples/lib/{{project_id}}.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * {{titlecase project_id}} is the enchilada.
+ */
+class {{titlecase project_id}} {
+
+  /**
+   * Construct {{titlecase project_id}} object.
+   */
+  constructor() {
+  }
+
+}
+
+export {
+  {{titlecase project_id}} as default
+};
